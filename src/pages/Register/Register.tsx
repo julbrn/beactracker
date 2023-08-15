4bcd22bb604ffb875cf191871407d877a6c6de61
Yulia Burenko <juliaburenko@yandex.ru>
2023-08-15 20:44:34 +0300

    feat: avatar selection

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./register.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 type Account = {
   username: string;
@@ -8,13 +8,32 @@ type Account = {
 };
 
 function Register() {
+  const [avatars, setAvatars] = useState<string[]>([]);
+  const [selectedAvatar, setSelectedAvatar] = useState<number | undefined>(undefined);
+  const api = "https://api.dicebear.com/6.x/adventurer-neutral/svg";
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm<Account>({mode: "onBlur"});
   const onSubmit = handleSubmit((data) => {});
+
+  const generateAvatars = () => {
+    const promises: Promise<Response>[] = [];
+    for (let i = 0; i < 3; i++) {
+      promises.push(fetch(`${api}?seed=${Math.round(Math.random() * 1000)}`));
+    }
+    Promise.all(promises).then((responses: Response[])  => {
+      const urls: string[] = responses.map((res) => res.url);
+      setAvatars(urls);
+    });
+  };
+
   console.log(errors);
+
+  useEffect(() => {
+    generateAvatars();
+  }, []);
 
   return (
     <div className="auth">
@@ -55,7 +74,26 @@ function Register() {
           name="password"
         />
         <p className="auth__error">{errors.password?.message}</p>
-        <button type="submit">Готово!</button>
+        <div className="auth__avatars-wrapper">
+        <h4 className="auth__avatars-title">Выберите аватар</h4>
+        <div className="auth__avatars">
+              {avatars.map((avatar, index) => {
+                return (
+                    <img
+                    key={index} className={`auth__avatar ${selectedAvatar === index ? "selected" : ""}`}
+                      src={avatar}
+                      alt="avatar"
+                      onClick={() => setSelectedAvatar(index)}
+                    />
+                );
+              })}
+   
+            </div>
+            <button className="generate-btn" onClick={generateAvatars} type="button">
+                Ещё
+              </button>
+        </div>
+        <button disabled={errors.username || errors.password ? true : false} type="submit">Готово!</button>
         <span>
           Уже есть аккаунт? <Link to="/">Войти</Link>
         </span>
