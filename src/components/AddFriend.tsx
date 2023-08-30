6dee959d645d32c01778c7bc76c2363705f9d6ad
Yulia Burenko <juliaburenko@yandex.ru>
2023-08-30 12:59:59 +0300

    fix: textfields in Profile component

---
+++ src/components/AddFriend.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Dialog, Button, DialogActions, DialogContent, TextField } from '@mui/material';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useForm } from "react-hook-form";
+
+type AddFriendProps = {
+    setFriendModalOpened: React.Dispatch<React.SetStateAction<boolean>>;
+    setFriendList: React.Dispatch<React.SetStateAction<string[]>>;
+    friendList: string[];
+    friendModalOpened: boolean;
+};
+
+function AddFriend({ setFriendModalOpened, setFriendList, friendList, friendModalOpened }: AddFriendProps) {
+    const {
+        register,
+        formState: { errors },
+    } = useForm({ mode: "onChange" });
+    const handleCloseFriendModal = () => {
+        setFriendModalOpened(false)
+    }
+    const handleAddFriend = (e) => {
+        e.preventDefault();
+        const friendName = (document.getElementById("friendName") as HTMLInputElement)?.value;
+        setFriendList([...friendList, friendName]);
+        handleCloseFriendModal();
+    }
+    return (
+        <Dialog open={friendModalOpened} onClose={handleCloseFriendModal}>
+            <form onSubmit={handleAddFriend}>
+                <DialogTitle>Добавьте человека</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Добавьте человека, который может Вам помочь выполнить это дело.
+                    </DialogContentText>
+                    <TextField
+                        margin="dense"
+                        id="friendName"
+                        label="Имя человека"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        error={!!errors.friend}
+                        helperText={errors.friend?.message}
+                        {...register("friend",
+                            {
+                                required: "Обязательное поле",
+                                minLength: { value: 2, message: "Имя должно содержать не менее 2 символов" }, pattern: { value: /^[A-Za-zА-Яа-я ]{2,20}$/, message: "Имя должно содержать только буквы" }
+                            })}
+                        name="friend"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseFriendModal}>Отмена</Button>
+                    <Button type="submit" >Добавить</Button>
+                </DialogActions>
+            </form>
+        </Dialog>
+    )
+}
+
+export default AddFriend
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { TextField, Dialog, Stack, Chip, DialogActions, DialogContent, Button, Tooltip, Fab, Select, Box, InputLabel, FormControl, MenuItem, Typography } from '@mui/material';
+import { TextField, Stack, Chip, Button, Tooltip, Fab, Select, Box, InputLabel, FormControl, MenuItem, Typography } from '@mui/material';
 import "./profile.css";
 import AddIcon from '@mui/icons-material/Add';
 import dayjs from "dayjs";
@@ -12,15 +12,8 @@ import Calendar from "../../components/Calendar/Calendar";
 import PersonAddAltRoundedIcon from '@mui/icons-material/PersonAddAltRounded';
 import ColouredSlider from "../../components/StyledSlider";
 import MoodRating from "../../components/MoodRating";
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import { useForm } from "react-hook-form";
-
-
-type Fields = {
-  friend: string;
-  activity: string;
-};
+import AddFriend from "../../components/AddFriend";
 
 function Profile() {
   const [diaryDate, setDiaryDate] = useState<string>(dayjs(new Date()).format("D MMMM"));
@@ -30,7 +23,7 @@ function Profile() {
   const {
     register,
     formState: { errors },
-  } = useForm<Fields>({ mode: "onBlur" });
+  } = useForm({ mode: "onChange" });
 
   const handleDiaryDateChange = (date: string) => {
     setDiaryDate(date);
@@ -48,14 +41,8 @@ function Profile() {
     setFriendModalOpened(true)
   }
 
-  const handleCloseFriendModal = () => {
-    setFriendModalOpened(false)
-  }
-
-  const handleAddFriend = () => {
-    const friendName = (document.getElementById("friendName") as HTMLInputElement)?.value;
-    setFriendList([...friendList, friendName]);
-    handleCloseFriendModal();
+  const handleSaveActivity = () => {
+    handleCloseActivityForm()
   }
 
   const handleFriendDelete = (friend: string) => {
@@ -68,7 +55,7 @@ function Profile() {
 
   return (
     <div className="profile">
-      <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 3, marginBottom: 20 }}>
         <Typography variant='h3'>{diaryDate}</Typography>
         <MoodRating />
         <Tooltip title="Добавить новую активность" placement="right">
@@ -77,113 +64,90 @@ function Profile() {
           </Fab>
         </Tooltip>
         {formOpened &&
-          <FormControl component="fieldset" sx={{ width: 850, display: "flex", gap: 3, backgroundColor: "#393f4b", border: "1px solid rgba(255, 255, 255, .2)", padding: 3, borderRadius: "5px" }}>
-            <TextField label="Активность" required type="text" style={{ width: 300, fontSize: 14 }} margin="normal" variant="outlined" error={!!errors.activity}
-              helperText={errors.activity?.message}
-              {...register("activity",
-                {
-                  required: "Обязательное поле",
-                  minLength: { value: 2, message: "Описание дела должно содержать не менее 2 символов" }, pattern: { value: /^[A-Za-zА-Яа-я0-9-_ ]{2,30}$/, message: "Недопустимый формат" }
-                })}
-              name="activity" />
-            <FormControl sx={{ width: 200 }} required >
-              <InputLabel shrink id="demo-simple-select-label">Сфера</InputLabel>
-              <Select variant='outlined' label="Настроение&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" notched sx={{ fontSize: 14 }}
-              >
-                <MenuItem sx={{ height: "50px", fontSize: 14 }} value={4}><div className='align'><PeopleRoundedIcon sx={{ marginRight: "3px" }} /><span>Отношения</span></div></MenuItem>
-                <MenuItem sx={{ height: "50px", fontSize: 14 }} value={3}><div className='align'><WorkOutlineRoundedIcon sx={{ marginRight: "3px" }} /><span>Образование/Карьера</span></div></MenuItem>
-                <MenuItem sx={{ height: "50px", fontSize: 14 }} value={2}><div className='align'><DownhillSkiingRoundedIcon sx={{ marginRight: "3px" }} /><span>Отдых/Интересы</span></div></MenuItem>
-                <MenuItem sx={{ height: "50px", fontSize: 14 }} value={1}><div className='align'><SelfImprovementIcon sx={{ marginRight: "3px" }} /><span>Разум/Тело/Духовность</span></div></MenuItem>
-                <MenuItem sx={{ height: "50px", fontSize: 14 }} value={0}><div className='align'><HouseRoundedIcon sx={{ marginRight: "3px" }} /><span>Быт</span></div></MenuItem>
-              </Select>
-            </FormControl>
-            <div style={{ width: '700px' }}>
-              <table style={{ width: '100%', borderSpacing: "30px" }}>
-                <thead>
-                  <tr>
-                    <th>Ожидание</th>
-                    <th>Реальность</th>
-                    <th>Мастерство</th>
-                    <th>Сложность</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <ColouredSlider
+          <form onSubmit={handleSaveActivity}>
+            <FormControl component="fieldset" sx={{ width: 850, display: "flex", gap: 3, backgroundColor: "#393f4b", border: "1px solid rgba(255, 255, 255, .2)", padding: 3, borderRadius: "5px" }}>
+              <TextField label="Активность" required type="text" style={{ width: 300, fontSize: 14 }} margin="normal" variant="outlined" error={!!errors.activity}
+                helperText={errors.activity?.message}
+                {...register("activity",
+                  {
+                    required: "Обязательное поле",
+                    minLength: { value: 2, message: "Описание дела должно содержать не менее 2 символов" }, pattern: { value: /^[A-Za-zА-Яа-я0-9-_ ]{2,30}$/, message: "Недопустимый формат" }
+                  })}
+                name="activity" />
+              <FormControl sx={{ width: 250 }} required >
+                <InputLabel shrink id="demo-simple-select-label">Сфера</InputLabel>
+                <Select variant='outlined' label="Настроение&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" notched sx={{ fontSize: 14 }}
+                >
+                  <MenuItem sx={{ height: "50px", fontSize: 14 }} value={4}><div className='align'><PeopleRoundedIcon sx={{ marginRight: "3px" }} /><span>Отношения</span></div></MenuItem>
+                  <MenuItem sx={{ height: "50px", fontSize: 14 }} value={3}><div className='align'><WorkOutlineRoundedIcon sx={{ marginRight: "3px" }} /><span>Образование/Карьера</span></div></MenuItem>
+                  <MenuItem sx={{ height: "50px", fontSize: 14 }} value={2}><div className='align'><DownhillSkiingRoundedIcon sx={{ marginRight: "3px" }} /><span>Отдых/Интересы</span></div></MenuItem>
+                  <MenuItem sx={{ height: "50px", fontSize: 14 }} value={1}><div className='align'><SelfImprovementIcon sx={{ marginRight: "3px" }} /><span>Разум/Тело/Духовность</span></div></MenuItem>
+                  <MenuItem sx={{ height: "50px", fontSize: 14 }} value={0}><div className='align'><HouseRoundedIcon sx={{ marginRight: "3px" }} /><span>Быт</span></div></MenuItem>
+                </Select>
+              </FormControl>
+              <div style={{ width: '700px' }}>
+                <table style={{ width: '100%', borderSpacing: "30px" }}>
+                  <thead>
+                    <tr>
+                      <th>Ожидание</th>
+                      <th>Реальность</th>
+                      <th>Мастерство</th>
+                      <th>Сложность</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <ColouredSlider
 
-                      /></td>
-                    <td>
-                      <ColouredSlider
+                        /></td>
+                      <td>
+                        <ColouredSlider
 
-                      /></td>
-                    <td>
-                      <ColouredSlider
+                        /></td>
+                      <td>
+                        <ColouredSlider
 
-                      /></td>
-                    <td>
-                      <ColouredSlider
+                        /></td>
+                      <td>
+                        <ColouredSlider
 
-                      /></td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
-              <Typography>Вам кто-то может помочь?</Typography>
-              <Fab color="primary" onClick={handleOpenFriendModal} aria-label="Добавить помощника" sx={{ width: "34px", height: "34px" }}>
-                <PersonAddAltRoundedIcon sx={{ width: "18px", height: "18px" }} />
-              </Fab>
-            </Box>
-            <Stack direction="row" spacing={1}>
-              {friendList.map((friend) => (
-                <Chip
-                  key={friend}
-                  label={friend}
-                  onDelete={() => handleFriendDelete(friend)}
-                  sx={{ fontSize: "16px", backgroundColor: "rgba(252, 174, 188, 0.6)" }}
-                />
-              ))}
-            </Stack>
-            <Dialog open={friendModalOpened} onClose={handleCloseFriendModal}>
-              <form onSubmit={(e) => { e.preventDefault() }}>
-                <DialogTitle>Добавьте человека</DialogTitle>
-                <DialogContent>
-                  <DialogContentText>
-                    Добавьте человека, который может Вам помочь выполнить это дело.
-                  </DialogContentText>
-                  <TextField
-                    margin="dense"
-                    id="friendName"
-                    label="Имя человека"
-                    type="text"
-                    fullWidth
-                    variant="standard"
-                    error={!!errors.friend}
-                    helperText={errors.friend?.message}
-                    {...register("friend",
-                      {
-                        required: "Обязательное поле",
-                        minLength: { value: 2, message: "Имя должно содержать не менее 2 символов" }, pattern: { value: /^[A-Za-zА-Яа-я ]{2,20}$/, message: "Имя должно содержать только буквы" }
-                      })}
-                    name="friend"
+                        /></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+                <Typography>Вам кто-то может помочь?</Typography>
+                <Fab color="primary" onClick={handleOpenFriendModal} aria-label="Добавить помощника" sx={{ width: "34px", height: "34px" }}>
+                  <PersonAddAltRoundedIcon sx={{ width: "18px", height: "18px" }} />
+                </Fab>
+              </Box>
+              <Stack direction="row" spacing={1}>
+                {friendList.map((friend) => (
+                  <Chip
+                    key={friend}
+                    label={friend}
+                    onDelete={() => handleFriendDelete(friend)}
+                    sx={{ fontSize: "16px", backgroundColor: "rgba(252, 174, 188, 0.6)" }}
                   />
-                </DialogContent>
-                <DialogActions>
-                  <Button onClick={handleCloseFriendModal}>Отмена</Button>
-                  <Button onClick={handleAddFriend} type="submit">Добавить</Button>
-                </DialogActions>
-              </form>
-            </Dialog>
-            <Box sx={{ display: "flex", gap: 3 }}>
-              <Button variant="outlined" onClick={handleCloseActivityForm}>Отмена</Button>
-              <Button variant="outlined" onClick={handleCloseActivityForm}>Добавить</Button>
-            </Box>
-          </FormControl>
+                ))}
+              </Stack>
+              <TextField id="comment" name="comment"
+                label="Комментарий"
+                multiline
+                rows={3}
+              />
+              <Box sx={{ display: "flex", gap: 3 }}>
+                <Button variant="outlined" onClick={handleCloseActivityForm}>Отмена</Button>
+                <Button variant="outlined" type="submit">Добавить</Button>
+              </Box>
+            </FormControl>
+          </form>
         }
       </Box>
       <Calendar onDiaryDateChange={handleDiaryDateChange} />
-
+      <AddFriend setFriendModalOpened={setFriendModalOpened} setFriendList={setFriendList} friendList={friendList} friendModalOpened={friendModalOpened} />
     </div>
   )
 }
