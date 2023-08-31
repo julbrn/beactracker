c5db9433ac52382e123cd453b5dcd95afda646e4
Yulia Burenko <juliaburenko@yandex.ru>
2023-08-31 12:45:47 +0300

    fix: change the color of the last slider

--- src/components/StyledSlider.tsx
+++ src/components/StyledSlider.tsx
@@ -17,11 +17,7 @@ function ColouredSlider({ name, value, handleChange }: ColouredSliderProps) {
             defaultValue={5}
             min={1}
             max={10}
-            sx={{
-                '& .MuiSlider-thumb, & .MuiSlider-track': {
-                    backgroundColor: value < 3 ? '#cf5353' : value < 5 ? '#ef9a61' : value < 7 ? '#ecdd82' : value < 9 ? '#b7ed71' : '#5bbc62'
-                },
-            }}
+            sx={name !== "difficulty" ? { '& .MuiSlider-thumb, & .MuiSlider-track': { backgroundColor: value < 3 ? '#cf5353' : value < 5 ? '#ef9a61' : value < 7 ? '#ecdd82' : value < 9 ? '#b7ed71' : '#5bbc62' } } : {}}
         />
     );
 }
